aeb99aa3e8608e373c1e10af4cd32c25cdfab718
Andres Sanabria <sanabria80.andres@gmail.com>
2022-09-09 01:33:02 -0500

    terminando M1

--- proyecto_M1/evaluacion.py
+++ proyecto_M1/evaluacion.py
@@ -5,9 +5,6 @@
 #
 #print(vel_en_caida_libre(98))
 
-
-
-from calendar import month
 from datetime import datetime, timedelta
 
 
@@ -19,14 +16,37 @@ from datetime import datetime, timedelta
 #    return f"{edad.years},{edad.months},{edad.days}"
 #
 #print(calcular_edad(20, 11, 1986, 16, 10, 1987))
-def calcular_edad(dia_nacio: int, mes_nacio: int, anio_nacio: int, dia_actual: int, mes_actual: int, anio_actual: int)->str:
-    fecha_nacimiento = datetime.strptime(f"{dia_nacio}/{mes_nacio}/{anio_nacio}", "%d/%m/%Y")
-    fecha_actual = datetime.strptime(f"{dia_actual}/{mes_actual}/{anio_actual}", "%d/%m/%Y")
-    diferencia = fecha_actual-fecha_nacimiento
-    diferencia = fecha_actual-fecha_nacimiento
-    anios = diferencia.days//365
-    meses = (diferencia.days - (anios*365)) //30.417
-    dias = diferencia.days - (anios*365) - (meses*30)
-    return f"{anios}, {meses:.0f}, {dias:.0f}"
 
-print(calcular_edad(20, 11, 1986, 16, 10, 1987))
+#def calcular_edad(dia_nacio: int, mes_nacio: int, anio_nacio: int, dia_actual: int, mes_actual: int, anio_actual: int)->str:
+#    fecha_nacimiento = datetime.strptime(f"{dia_nacio}/{mes_nacio}/{anio_nacio}", "%d/%m/%Y")
+#    fecha_actual = datetime.strptime(f"{dia_actual}/{mes_actual}/{anio_actual}", "%d/%m/%Y")
+#    diferencia = fecha_actual-fecha_nacimiento
+#
+#    anios = diferencia.days//360
+#    meses = (diferencia.days - (anios*360))//30.417
+#    dias = diferencia.days - (anios*360) - (meses*30)
+#    return f"{anios}, {meses:.0f}, {dias:.0f}"
+#
+#print(calcular_edad(20, 11, 1986, 16, 10, 1987))
+
+def calcular_edad(dia_nacio: int, mes_nacio: int, anio_nacio: int, dia_actual: int, mes_actual: int, anio_actual: int)->str:
+    diasTotalesActuales = dia_actual +  (anio_actual*360) + (mes_actual*30)
+    diasTotales =  dia_nacio + (anio_nacio*360) + (mes_nacio*30)
+    diasEdad = diasTotalesActuales - diasTotales
+    anios = 0
+    meses = 0
+    dias = 0
+    while diasEdad >= 360:
+        anios += 1
+        diasEdad -= 360
+    while diasEdad >= 30:
+        meses += 1
+        diasEdad -= 30
+
+    dias = diasEdad
+    return f"{anios},{meses},{dias}"
+
+
+print(calcular_edad(25, 10, 1993, 4, 8, 2019))
+
+
--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-def  calcular_BMI(peso_lb: float, altura_inch: float) -> float:
-    result = (peso_lb*0.45)/(altura_inch*0.025)**2
-    return round(result, 2)
+#var1 = '4'
+#var2 = int(str(var1))
+#var3 = var1*2
+#var1 = var2*2
+#var2 = var1
+#var1 = str(float(var2))
 
-print(calcular_BMI(220, 78))
+#print(var1) # 8.0
+
+var1 = 1234
+var2 = str(var1)
+var3 = float()
+var1 = var2*2
+var2 = var1
+var1 = str(float(var2))
